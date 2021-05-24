01b537e4846218ef8a7d390476e8cda4b5364b56
lcw <lcw@lcw.com>
2021-05-25 07:52:33 +0800

    2021/5/25/8

--- jd_zoo.js
+++ jd_zoo.js
@@ -20,17 +20,14 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let cookiesArr = [], cookie = '',secretp = '',
 //商圈码
 shareCodeList = [
-  'sSKNX-MpqKPS5LC_m5vbBzBzHaADE-9mL5PCLBruARiICqDqifUKezxvgTpXzTk',
-  'sSKNX-MpqKPS4b24nJjZB61Vndtg4qJ_VG7HRFgF6TbLLDLuCOX6EMc',
-  'sSKNX-MpqKMFeg9hMgEOk83UPofavDwk_ALr8Dq6tEDNkdTnDRly',
-  'sSKNX-MpqKMHbDJtODsMol7N0NWkihQrlNa8pa6sWgXSYOfj8o5m4bqb-qqB'
+  '',
+  '',
+  '',
+  ''
 ];
 //个人码
 inviteIdList = [
-  'ZXTKT022v_13RxwZ91ffPR_wlPcNfAFjRWn6-7zx55awQ',
-  'ZXTKT018v_h6QBsa9VfeKByb1AFjRWn6-7zx55awQ',
-  'ZXTKT016aGPImbWDIsNs9Zd1FjRWn6-7zx55awQ',
-  'ZXTKT020anX1lb-5IPJt9JJyQH-MFjRWn6-7zx55awQ'
+  'ZXTKT022v_13RxwZ91ffPR_wlPcNfAFjRWn6-7zx55awQ','ZXTKT018v_h6QBsa9VfeKByb1AFjRWn6-7zx55awQ','ZXTKT016aGPImbWDIsNs9Zd1FjRWn6-7zx55awQ','ZXTKT020anX1lb-5IPJt9JJyQH-MFjRWn6-7zx55awQ','ZXTKT018v_h6Rhof_FzTIxyb1AFjRWn6-7zx55awQ','ZXTKT0205KkcNmJNlxOBRUCU9axOFjRWn6-7zx55awQ','ZXTKT0225KkcRx9Pp1eCdRuhkvNcdQFjRWn6-7zx55awQ','ZXTKT0205KkcB2FFpxCqQW6K0qFfFjRWn6-7zx55awQ'
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
