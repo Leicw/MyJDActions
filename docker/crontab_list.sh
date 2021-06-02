15f1a72404f03189417cc3cf5bc64ee7d5955def
lcw <lcw@lcw.com>
2021-06-03 07:45:58 +0800

    2021/6/3/8

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -28,8 +28,6 @@
 0,30 0-23/1 * * * node /scripts/jd_live_redrain.js >> /scripts/logs/jd_live_redrain.log 2>&1
 #女装盲盒 活动时间：2021-05-1到2021-05-31
 35 1,22 * * * node /scripts/jd_nzmh.js >> /scripts/logs/jd_nzmh.log 2>&1
-#京享值pk 活动时间：2021-05-17到2021-05-31
-15 10,15,18 17-31 5 * node /scripts/jd_pk.js >> /scripts/logs/jd_pk.log 2>&1
 #母婴跳一跳 
 1 0,11,21 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #人头马
@@ -43,9 +41,13 @@
 #动物联盟
 0 0,6-23/2 * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
 #动物联盟收集金币
-0-59/30 * * * * node /scripts/jd_zoo.js >> /scripts/logs/jd_zoo.log 2>&1
+0-59/30 * * * * node /scripts/jd_zooCollect.js >> /scripts/logs/jd_zooCollect.log 2>&1
 #星推官
 0 0 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
+#jd_babelDiy
+30 12 * * * node /scripts/jd_babelDiy.js >> /scripts/logs/jd_babelDiy.log 2>&1
+#星店长
+5 7 * * * node /scripts/jd_starStore.js >> /scripts/logs/jd_starStore.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
